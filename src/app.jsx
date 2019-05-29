e92854a26a06a9aa7d2dc466df15c29e9ff04069
Dante Moore <dante@Marcos-MBP.localdomain>
2019-05-29 12:06:46 -0700

    corrected merge conflict

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,8 @@
 import React from "react";
 import EmoSpherO from "./components/EmoSpherO";
-<<<<<<< HEAD
 
 import MenuExampleStackable from "./components/Header";
 const sphero = require("./mySphero");
-=======
->>>>>>> 5f245d36e5c8c57447b276e6cac2d33a94258530
 
 export default class App extends React.Component {
   render() {
