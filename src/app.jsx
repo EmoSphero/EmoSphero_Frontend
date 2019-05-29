2828d504da2282692ff8994d434581cf2b1fa29b
Dante Moore <dante@Marcos-MBP.localdomain>
2019-05-29 11:54:31 -0700

    electron and semantic added

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import EmoSpherO from "./components/EmoSpherO";
+
+import MenuExampleStackable from "./components/Header";
 const sphero = require("./mySphero");
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
+        <MenuExampleStackable />
+
         <h1>Welcome to EmoSpherO</h1>
         <EmoSpherO />
       </div>
